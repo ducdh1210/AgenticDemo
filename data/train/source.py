461c76c204808a58ea936bc16c63eb48ab9b6b75
Duc Do <ducdh1210@gmail.com>
2024-08-27 09:00:45 -0500

    untrack files

--- data/train/source.py
+++
@@ -1,9 +0,0 @@
-urls = [
-    "https://www.usebutton.com/",
-    "https://www.usebutton.com/retail-media",
-    "https://www.usebutton.com/for-publishers",
-    "https://www.usebutton.com/for-retailers",
-    "https://www.usebutton.com/for-creators",
-    "https://www.usebutton.com/button-marketplace",
-    "https://www.usebutton.com/post/sams-club-experienced-a-165-increase-in-total-mobile-affiliate-revenue-with-buttons-reach",
-]
